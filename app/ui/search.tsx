4bfb0f6e13396c48e9263a1d53d050144dd579f0
lajimi mahmoud <mahmoudladjimi@gmail.com>
2024-01-09 14:08:00 +0100

    search add pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -7,19 +7,17 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
+  
   const handleSearch = useDebouncedCallback((term) => {
-    console.log(`Searching... ${term}`)
-  function handleSearch(term: string) {
-   
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
- 
-  } }, 300);
+  }, 300);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
