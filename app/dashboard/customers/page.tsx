bd610ac81b6c3b626c982d9d438c8f3f15fe8b9e
lajimi mahmoud <mahmoudladjimi@gmail.com>
2024-01-10 17:11:34 +0100

    Practice: Adding metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,12 @@
 import React from "react";
-
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Custoers | Lajimi Dashboard',
+};
 export default function Page() {
     return ( 
-    <p> Olla Sweet Customers Page </p>
+    <p> Olla Sweet Customer💗 </p>
 
     );
   }
